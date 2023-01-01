d7bb8dff9bdad0c6c6598353025b8ed3043b70d3
LazezBZH <lazez.bzh@caramail.fr>
2023-01-01 22:54:55 +0100

    pwa4

--- register-sw.js
+++ register-sw.js
@@ -1,8 +1,29 @@
+// if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", () => {
+//     navigator.serviceWorker.register("/service-worker.js");
+//   });
+// }
+
 if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js");
-  });
+  navigator.serviceWorker
+    .register("service-worker.js", {
+      scope: ".",
+    })
+    .then(
+      function (registration) {
+        // Registration was successful
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      function (err) {
+        // registration failed :(
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
 }
+
 self.addEventListener("install", (event) => {
   console.log("Inside the install handler:", event);
 });
